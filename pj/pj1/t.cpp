885baeec677347fe97b0c9d838d4e24afbcc5905
Ilya193 <40058340+Ilya193@users.noreply.github.com>
2019-07-31 18:45:04 +0300

    Update t.cpp

--- pj/pj1/t.cpp
+++ pj/pj1/t.cpp
@@ -31,20 +31,22 @@ void Pj::general_fun()
 void Pj::fill()
 {
     std::cout << "number: ";
-
     if (!(std::cin >> tempn))
     {
         std::cout << "not a number" << std::endl;
         exit(0);
     }
-
     std::cin.get();
-
     numbers[count] = tempn;
 
     std::cout << "name: ";
     getline(std::cin, temps);
-
+    if (temps[0] == '1' || temps[0] == '2' || temps[0] == '3' || temps[0] == '4' || temps[0] == '5' ||
+    temps[0] == '6' || temps[0] == '7' || temps[0] == '8' || temps[0] == '9')
+    {
+        std::cout << "string!" << std::endl;
+        Pj::fill();
+    }
     name[count] = temps;
 
     count++;
@@ -54,13 +56,17 @@ void Pj::fill()
 
 void Pj::rewrite()
 {
-    std::cout << "n - number\ns - name" << std::endl;
+    std::cout << "n - number\ns - name\ne - exit" << std::endl;
     std::cin >> ch;
 
     if (ch == 'n')
     {
         std::cout << "change the number: ";
-        std::cin >> tempn;
+        if (!(std::cin >> tempn))
+        {
+            std::cout << "not a number" << std::endl;
+            exit(0);
+        }
         std::cin.get();
 
         for (int key = 0; key < count; key++)
@@ -68,9 +74,12 @@ void Pj::rewrite()
             if (tempn == numbers[key])
             {
                 std::cout << "enter the new number: ";
-                std::cin >> new_number;
+                if (!(std::cin >> new_number))
+                {
+                    std::cout << "not a number" << std::endl;
+                    exit(0);
+                }
                 std::cin.get();
-
                 numbers[key] = new_number;
 
                 general_fun();
@@ -95,7 +104,6 @@ void Pj::rewrite()
              {
                  std::cout << "enter the new name: ";
                  getline(std::cin, new_name);
-
                  name[key] = new_name;
 
                  general_fun();
@@ -104,12 +112,22 @@ void Pj::rewrite()
              if (cl == 0 && key == count - 1)
              {
                  std::cout << "no name." << std::endl;
+
  		         std::cout << "coincidences: ";
                  coincidences();
-                 std::cout << std::endl;
+
+                 if (c > 0)
+                 {
+                     std::cout << "no" << std::endl;
+                 }
+
                  rewrite();
              }
          }
+     }
+     else if (ch == 'e')
+     {
+ 	    exit(0);
      }
      else
      {
@@ -139,18 +157,36 @@ void Pj::coincidences()
     }
 }
 
-void Pj::ch_b_char()
+bool Pj::ch_b_char()
 {
-    for (int key = 0; key < temps.size(); key++)
+    if (temps.size() > t.size())
     {
-        if (temps[key] == t[key])
+        return 0;
+    }
+    else
+    {
+        for (int key = 0; key < temps.size(); key++)
         {
-            ccs += 1;
+            for (int i = 0; i < t.size(); i++)
+            {
+                if (temps[key] == t[i])
+                {
+                    ccs += 1;
+                }
+            }
 
             if (ccs >= 2)
             {
-                std::cout << t << ", ";
+                std::cout << t << std::endl;
+                break;
             }
         }
     }
+
+    if (ccs == 0)
+    {
+        c += 1;
+    }
+
+    ccs = 0;
 }
