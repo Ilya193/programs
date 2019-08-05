ebaaee810f2ea45a9f958eef2b3f95fac8bd017f
Ilya193 <40058340+Ilya193@users.noreply.github.com>
2019-08-05 13:36:58 +0300

    Update z.h

--- pj/pj2/z.h
+++ pj/pj2/z.h
@@ -2,9 +2,9 @@ class Str
 {
 private:
     int* num_chs;
-    int  gap = 0, num_ch = 0;
-    int ch = 0, var_arr = 0, num = 0, gaps = 0;
+    int  gap = 0, num_ch = 0, ch = 0, var_arr = 0, num = 0, gaps = 0;
 
 public:
     int strlen(char*);
+    Str(int, int, int, int, int, int);
 };
