e0ae54b6b4ec98a055ff0f98a82a1f3f658dd828
Ilya193 <40058340+Ilya193@users.noreply.github.com>
2019-08-08 21:45:20 +0300

    Update main.cpp

--- pj/pj5/main.cpp
+++ pj/pj5/main.cpp
@@ -4,35 +4,33 @@
 
 int main()
 {
-	std::ifstream file1, file2;
-	file1.open("g");
+    std::ifstream file1, file2;
+    file1.open("g");
 
-	char temp[80];
-	int count = 0;
+    char temp[80];
+    int count = 0;
 
-	while (file1 >> temp)
-	{
-		count++;
-	}
+    while (file1 >> temp)
+    {
+	count++;
+    }
 
-	file1.close();
+    file1.close();
 
-	Lesson c = Lesson(count);
-	file2.open("g");
+    Lesson c = Lesson(count);
+    file2.open("g");
+	
+    while (file2 >> temp)
+    {
+    	c << temp;
+    }
 
-	int i = 0;
-	while (file2 >> temp)
-	{
- 	    c << temp;
-		i++;
-	}
+    for (int key = 0; key < count; key++)
+    {
+	std::cout << c << std::endl;
+    }
 
-	for (int key = 0; key < count; key++)
-	{
-		std::cout << c << std::endl;
-	}
-
-	file2.close();
+    file2.close();
 
     return 0;
 }
