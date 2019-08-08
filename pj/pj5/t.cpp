b5cdeb105ed9303b92db3a18c9b1e5f5be8a6081
Ilya193 <40058340+Ilya193@users.noreply.github.com>
2019-08-08 21:46:06 +0300

    Update t.cpp

--- pj/pj5/t.cpp
+++ pj/pj5/t.cpp
@@ -2,36 +2,36 @@
 
 Lesson::Lesson()
 {
-	str_file = new std::string[0];
+    str_file = new std::string[0];
 }
 
 Lesson::Lesson(int size)
 {
-	str_file = new std::string[size];
+    str_file = new std::string[size];
 }
 
 Lesson::~Lesson()
 {
-	delete [] str_file;
+    delete [] str_file;
 }
 
 void Lesson::operator<<(const std::string& ipt)
 {
-	this->str_file[key] = ipt;
+    this->str_file[key] = ipt;
 
-	key++;
+    key++;
 }
 
 std::ostream& operator<<(std::ostream& os, const Lesson& l)
 {
-	int temp = 0;
+    int temp = 0;
 
-	if (temp < l.key)
-	{	
+    if (temp < l.key)
+    {	
     	os << l.str_file[temp];
-	}
+    }
 
-	temp++;
+    temp++;
 
 	return os;
 }
