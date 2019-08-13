9efdb9bcd7d2d38c38a60c0474c072064b079b13
Ilya193 <40058340+Ilya193@users.noreply.github.com>
2019-08-13 17:37:52 +0300

    Update main.cpp

--- pj/pj6/main.cpp
+++ pj/pj6/main.cpp
@@ -3,18 +3,18 @@
 
 int main()
 {
-	cl t1 = cl("hell");
-	cl t2 = cl("hello");
+    cl t1 = cl("hell");
+    cl t2 = cl("hello");
 
-	char symbol1 = t1.operator[](3);
-	char symbol2 = t2.operator[](2);
+    char symbol1 = t1.operator[](3);
+    char symbol2 = t2.operator[](2);
 
-	std::cout << symbol1 << std::endl;
-	std::cout << symbol2 << std::endl;
+    std::cout << symbol1 << std::endl;
+    std::cout << symbol2 << std::endl;
 
-	t1.operator==(t2);
-	t1.operator>(t2);
-	t1.operator<(t2);
+    t1.operator==(t2);
+    t1.operator>(t2);
+    t1.operator<(t2);
 
     return 0;
 }
