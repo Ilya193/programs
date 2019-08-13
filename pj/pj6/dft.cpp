e503c979292de0b063fb6e791aa2740bc7375ac4
Ilya193 <40058340+Ilya193@users.noreply.github.com>
2019-08-13 17:37:23 +0300

    Update dft.cpp

--- pj/pj6/dft.cpp
+++ pj/pj6/dft.cpp
@@ -2,24 +2,24 @@
 
 cl::cl(const char* n)
 {
-	len = strlen(n);
+    len = strlen(n);
 
-	name = new char[len + 1];
+    name = new char[len + 1];
 
-	for (int key = 0; n[key] != '\0'; key++)
-	{
-		name[key] = n[key];
-	}
+    for (int key = 0; n[key] != '\0'; key++)
+    {
+	name[key] = n[key];
+    }
 }
 
 cl::~cl()
 {
-	delete [] name;
+    delete [] name;
 }
 
 int cl::strlen(const char* string)
 {
-	int length = 0, ch = 0;
+    int length = 0, ch = 0;
 
     while (string[length] != '\0')
     {
@@ -36,74 +36,77 @@ int cl::strlen(const char* string)
 
 char cl::operator[](int num)
 {
-	if (num > len)
-	{
-		return 'n';
-	}
+    if (num > len)
+    {
+	return 'n';
+    }
 
-	return name[num];
+    return name[num];
 }
 
 void cl::operator==(const cl& t2_)	
 {
-	int len1 = strlen(name);
-	char* name_met1 = new char[len1];
+    int len1 = strlen(name);
+    char* name_met1 = new char[len1];
 
-	for (int key = 0; name[key] != '\0'; key++)
-	{
-		name_met1[key] = name[key];
-	}
+    for (int key = 0; name[key] != '\0'; key++)
+    {
+	name_met1[key] = name[key];
+    }
 	
-	int len2 = strlen(t2_.name);
-	char* name_met2 = new char[len2];
+    int len2 = strlen(t2_.name);
+    char* name_met2 = new char[len2];
 
-	for (int key = 0; t2_.name[key] != '\0'; key++)
+    for (int key = 0; t2_.name[key] != '\0'; key++)
+    {
+	name_met2[key] = t2_.name[key];
+    }
+
+    int res = 0;
+
+    if (len1 != len2)
+    {
+	std::cout << "false" << std::endl;
+
+	delete [] name_met1;
+	delete [] name_met2;
+     }
+     else if (len1 == len2)
+     {
+	for (int key = 0; name_met1[key] != '\0'; key++)
 	{
-		name_met2[key] = t2_.name[key];
-	}
-
-	int res = 0;
-
-	if (len1 != len2)
-	{
+	    if (name_met1[key] != name_met2[key])
+	    {
 		std::cout << "false" << std::endl;
-
-		delete [] name_met1;
-		delete [] name_met2;
+	    }
+	    else if (name_met1[key] == name_met2[key])
+	    {
+		res++;
+	    }
 	}
-	else if (len1 == len2)
-	{
-		for (int key = 0; name_met1[key] != '\0'; key++)
-		{
-			if (name_met1[key] != name_met2[key])
-			{
-				std::cout << "false" << std::endl;
-			}
-			else if (name_met1[key] == name_met2[key])
-			{
-				res++;
-			}
-		}
 
-		if (res == len1) std::cout << "true" << std::endl;
-		else std::cout << "false" << std::endl;
-	}
+	if (res == len1) std::cout << "true" << std::endl;
+	else std::cout << "false" << std::endl;
+	    
+	delete [] name_met1;
+	delete [] name_met2;
+     }
 }
 
 void cl::operator>(const cl& t2_)
 {
-	int len1 = strlen(name);
-	int len2 = strlen(t2_.name);
+    int len1 = strlen(name);
+    int len2 = strlen(t2_.name);
 
-	if (len1 > len2) std::cout << "true" << std::endl;
-	else std::cout << "false" << std::endl;
+    if (len1 > len2) std::cout << "true" << std::endl;
+    else std::cout << "false" << std::endl;
 }
 
 void cl::operator<(const cl& t2_)
 {
-	int len1 = strlen(name);
-	int len2 = strlen(t2_.name);
+    int len1 = strlen(name);
+    int len2 = strlen(t2_.name);
 
-	if (len1 < len2) std::cout << "true" << std::endl;
-	else std::cout << "false" << std::endl;
+    if (len1 < len2) std::cout << "true" << std::endl;
+    else std::cout << "false" << std::endl;
 }
