9a0fb9e16238eaabc598782a1c9fad0ca5254533
Ilya193 <40058340+Ilya193@users.noreply.github.com>
2019-08-15 16:08:59 +0300

    Update main10.cpp

--- lite/main10.cpp
+++ lite/main10.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
-#include "dft.cpp"
+//#include "dft.cpp"
 
 int main()
 {
-	char str[6] = "$$$$$";
+    char str[6] = "$$$$$";
 
-	for (int key = 0; key < 5; key++)
+    for (int key = 0; key < 5; key++)
+    {
+	if (key == 0)
 	{
-		if (key == 0)
-		{
-			std::cout << str[key] << std::endl;
+	    std::cout << str[key] << std::endl;
 
-			continue;
-		}
-
-		for (int j = 0; j != key + 1; j++)
-		{
-			std::cout << str[j];
-		}
-
-		std::cout << std::endl;
+	    continue;
 	}
 
+	for (int j = 0; j != key + 1; j++)
+	{
+	    std::cout << str[j];
+	}
+
+	std::cout << std::endl;
+    }
+	
     return 0;
 }
