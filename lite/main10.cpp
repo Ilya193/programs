2b89fdd3d72fc357a0b15db26da28e5b27d7172e
Ilya193 <40058340+Ilya193@users.noreply.github.com>
2019-08-15 16:08:07 +0300

    Add files via upload

---
+++ lite/main10.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "dft.cpp"
+
+int main()
+{
+	char str[6] = "$$$$$";
+
+	for (int key = 0; key < 5; key++)
+	{
+		if (key == 0)
+		{
+			std::cout << str[key] << std::endl;
+
+			continue;
+		}
+
+		for (int j = 0; j != key + 1; j++)
+		{
+			std::cout << str[j];
+		}
+
+		std::cout << std::endl;
+	}
+
+    return 0;
+}
