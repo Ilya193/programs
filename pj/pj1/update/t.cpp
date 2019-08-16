aa1750a9b92b01705b8c99db4f6e5c6b7532fc29
Ilya193 <40058340+Ilya193@users.noreply.github.com>
2019-08-16 13:39:18 +0300

    Update t.cpp

--- pj/pj1/update/t.cpp
+++ pj/pj1/update/t.cpp
@@ -2,15 +2,16 @@
 
 Pj::Pj()
 {
-    count = 0;
-    cl = 0;
-    ccs = 0;
-    c = 0;
+	count = 0;
+	cl = 0;
+	ccs = 0;
+	c = 0;
     count_f = 0;
     ts = 0;
+    i = 0;
 
-    name = new std::string[200];
-    numbers = new int[200];
+	name = new std::string[200];
+	numbers = new int[200];
 }
 
 Pj::Pj(const Pj& t)
@@ -31,19 +32,19 @@ Pj::Pj(const Pj& t)
 
 Pj::~Pj()
 {
-    delete [] name;
-    delete [] numbers;
+	delete [] name;
+	delete [] numbers;
 }
 
 std::ostream& operator<<(std::ostream& os, const Pj& t2_)
 {
-    for (int key = 0; key < t2_.count; key++)
-    {
-	os << "number: " << t2_.numbers[key] << std::endl;
-	os << "name: " << t2_.name[key] << std::endl;
-    }
+	for (int key = 0; key < t2_.count; key++)
+	{
+        os << "name: " << t2_.name[key] << std::endl;
+		os << "number: " << t2_.numbers[key] << std::endl;
+	}
 
-    return os;
+	return os;
 }
 
 void Pj::general_fun()
@@ -67,6 +68,7 @@ void Pj::general_fun()
     else if (ch == 'e')
     {
 	    exit(0);
+        i = 0;
     }
     else if (ch == 'c')
     {
@@ -92,7 +94,7 @@ void Pj::fill()
         exit(0);       
     }
 
-    std::cout << "name -> number" << std::endl;
+    std::cout << "name -> number - > choise" << std::endl;
 
     while (content != ".")
     {
@@ -114,8 +116,11 @@ void Pj::fill()
 
 void Pj::rewrite()
 {
+    file_read();
+
     std::cout << "n - number\ns - name\ne - exit" << std::endl;
     std::cin >> ch;
+    std::cin.get();
 
     if (ch == 'n')
     {
@@ -123,9 +128,8 @@ void Pj::rewrite()
         if (!(std::cin >> tempn))
         {
             std::cout << "not a number" << std::endl;
-            exit(0);
+            general_fun();
         }
-        std::cin.get();
 
         for (int key = 0; key < count; key++)
         {
@@ -135,70 +139,72 @@ void Pj::rewrite()
                 if (!(std::cin >> new_number))
                 {
                     std::cout << "not a number" << std::endl;
-                    exit(0);
+                    rewrite();
                 }
                 std::cin.get();
+
                 numbers[key] = new_number;
 
+                spec_fun();
                 general_fun();
             }
-
             if (cl == 0 && key == count - 1)
             {
                 std::cout << "no number" << std::endl;
                 rewrite();
 	        }
-	     }
-     }
-     else if (ch == 's')
-     {
-         std::cout << "change the name: ";
-         std::cin.get();
-         getline(std::cin, temps);
+        }
+    }
+    else if (ch == 's')
+    {
+        std::cout << "change the name: ";
+        getline(std::cin, temps);
 
-         for (int key = 0; key < count; key++)
-         {
-             if (temps == name[key])
-             {
-                 std::cout << "enter the new name: ";
-                 getline(std::cin, new_name);
-                 name[key] = new_name;
+        for (int key = 0; key < count; key++)
+        {
+            std::cout << "temps: " << temps << std::endl;
+            if (temps == name[key])
+            {
+                std::cout << "enter the new name: ";
+                getline(std::cin, new_name);
+                name[key] = new_name;
+                spec_fun();
 
-                 general_fun();
+                general_fun();
              }
 
-             if (cl == 0 && key == count - 1)
-             {
-                 std::cout << "no name." << std::endl;
+            if (cl == 0 && key == count - 1)
+            {
+                std::cout << "no name." << std::endl;
 
- 		         std::cout << "coincidences: ";
-                 coincidences();
+ 		        std::cout << "coincidences: ";
+                coincidences();
 
-                 if (c > 0)
-                 {
-                     std::cout << "no" << std::endl;
-                 }
+                if (c > 0)
+                {
+                    std::cout << "no" << std::endl;
+                }
 
-                 rewrite();
+                rewrite();
              }
          }
-     }
-     else if (ch == 'e')
-     {
- 	    exit(0);
-     }
-     else
-     {
-         std::cout << "no ch" << std::endl;
-         rewrite();
-     }
+    }
+    else if (ch == 'e')
+    {
+ 	    general_fun();
+    }
+    else
+    {
+        std::cout << "no ch" << std::endl;
+        rewrite();
+    }
 }
 
 void Pj::display()
 {
-    Pj t2 = Pj(*this);
+	Pj t2 = Pj(*this);
 	
-    std::cout << t2;
+	std::cout << t2;
 
     general_fun();
 }
@@ -206,6 +212,7 @@ void Pj::display()
 void Pj::clear()
 {
     std::cout << "enter a name: ";
+    std::cin.get();
     std::cin.get();
     getline(std::cin, t);
 
@@ -220,13 +227,20 @@ void Pj::clear()
             }
 
             count--;
-            general_fun();
+            spec_fun();
+            break;
         }
         else if (t == name[key] && key + 1 == count)
         {
             count--;
+            break;
         }
     }
+    
+    count--;
+
+    spec_fun();
+    general_fun();
 }
 
 void Pj::coincidences()
@@ -304,19 +318,25 @@ void Pj::file_read()
             {
                 file_i.close();
                 fill();
-
             }
             else
             {
                 file_i.close();
+                
                 general_fun();
             }
         }
         else
         {
+            file_o.open("contacts", std::ios::app);
+            file_o << std::endl;
+            file_o.close();
+
+            i = 1;
+
             file_i.close();
             file_fill();
-            general_fun();
+            //general_fun();
         }
     }
 }
@@ -343,7 +363,7 @@ void Pj::file_fill()
         {
             numbers[count - ts] = atoi(content.c_str());
         }
-        else if (count_f % 2 != 0)
+        else
         {
             name[count - ts] = content;
             ts++;
@@ -356,4 +376,25 @@ void Pj::file_fill()
     file_i.close();
 
     count -= ts;
+}
+
+void Pj::spec_fun()
+{
+    int h = 1, l = 0, p = 0;
+
+    file_o.open("contacts");
+    if (!file_o.is_open())
+    {
+        std::cout << "file is not open" << std::endl;
+        file_i.close();
+        general_fun();        
+    }
+
+    for (int key = 0; key < count; key++)
+    {
+        file_o << name[key] << std::endl;
+        file_o << numbers[key] << std::endl;
+    }
+
+    file_o.close();
 }
