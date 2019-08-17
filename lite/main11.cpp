f596b577cd7796e23c07328b6bb175611876e4ec
Ilya193 <40058340+Ilya193@users.noreply.github.com>
2019-08-17 14:46:52 +0300

    Update main11.cpp

--- lite/main11.cpp
+++ lite/main11.cpp
@@ -4,20 +4,20 @@ void fib(int x, int y, int z);
 
 int main()
 {
-	int x = 1, y = 1, z = 0;
+    int x = 1, y = 1, z = 0;
 
-	fib(x, y, z);
+    fib(x, y, z);
 
 	return 0;
 }
 
 void fib(int x, int y, int z)
 {
-	while (z < 1000)
-	{
-		z = x + y;
-		std::cout << z << std::endl;
-		x = y;
-		y = z;
-	}
+    while (z < 1000)
+    {
+	z = x + y;
+	std::cout << z << std::endl;
+	x = y;
+	y = z;
+    }
 }
