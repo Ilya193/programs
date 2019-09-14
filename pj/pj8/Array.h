07e7b59757c0ba1fbeae8db0eb35180e50be6425
Ilya193 <40058340+Ilya193@users.noreply.github.com>
2019-09-14 21:09:09 +0300

    Update Array.h

--- pj/pj8/Array.h
+++ pj/pj8/Array.h
@@ -4,74 +4,74 @@ template <typename type, int val>
 class A
 {
 private:
-	type arr[val];
-	int top;
+    type arr[val];
+    int top;
 
 public:
-	A();
+    A();
 
-	void show() const;
-	void del(int i = val);
-	void append(int i = 0);
+    void show() const;
+    void del(int i = val);
+    void append(int i = 0);
 };
 
 template <typename type, int val>
 A<type, val>::A()
 {
-	top = val - 1;
+    top = val - 1;
 
-	for (int key = 0; key < top; key++)
-	{
-		arr[key] = key;
-	}
+    for (int key = 0; key < top; key++)
+    {
+        arr[key] = key;
+    }
 }
 
 template <typename type, int val>
 void A<type, val>::show() const
 {
-	std::cout << "arr: " << std::endl;
-	for (int key = 0; key < top; key++)
-	{
-		std::cout << arr[key] << std::endl;
-	}
+    std::cout << "arr: " << std::endl;
+    for (int key = 0; key < top; key++)
+    {
+        std::cout << arr[key] << std::endl;
+    }
 }
 
 template <typename type, int val>
 void A<type, val>::del(int i)
 {
-	if (i == val)
-	{
-		top--;
-	}
-	else
-	{
-		i--;
+    if (i == val)
+    {
+	top--;
+    }
+    else
+        {
+	    i--;
 
-		for (int x = 0; x <= i; x++)
+	    for (int x = 0; x <= i; x++)
+	    {
+		if (x == i)
 		{
-			if (x == i)
-			{
-				for (int y = x; y <= top; y++)
-				{
-					arr[y] = arr[y + 1];
-				}
-			}
+		    for (int y = x; y <= top; y++)
+		    {
+		        arr[y] = arr[y + 1];
+		    }
 		}
+	}
 
-		top--;
+	top--;
 	}
 }
 
 template <typename type, int val>
 void A<type, val>::append(int i)
 {
-	if (top == val - 1)
-	{
-		std::cout << "full" << std::endl;
-	}
-	else
-	{
-		arr[top] = i;
-		top++;
-	}
+    if (top == val - 1)
+    {
+	std::cout << "full" << std::endl;
+    }
+    else
+    {
+	arr[top] = i;
+	top++;
+    }
 }
