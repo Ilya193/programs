cd805bc6795323aad15164a980b242bfcec5aae0
Ilya193 <40058340+Ilya193@users.noreply.github.com>
2019-09-15 18:52:34 +0300

    Update Array.h

--- pj/pj8/Array.h
+++ pj/pj8/Array.h
@@ -13,6 +13,9 @@ public:
     void show() const;
     void del(int i = val);
     void append(int i = 0);
+    void min_number();
+    void max_number();
+    void sum_numbers();
 };
 
 template <typename type, int val>
@@ -74,4 +77,45 @@ void A<type, val>::append(int i)
 	top++;
 	arr[top] = i;
     }
+}
+
+template <typename type, int val>
+void A<type, val>::max_number()
+{
+    for (int key = 0; key <= top; key++)
+    {
+        if (arr[key] > max)
+        {
+            max = arr[key];
+        }
+    }
+
+    std::cout << "max: " << max << std::endl;
+}
+
+template <typename type, int val>
+void A<type, val>::min_number()
+{
+    min = arr[0];
+
+    for (int key = 0; key <= top; key++)
+    {
+        if (arr[key] < min)
+        {
+            min = arr[key];
+        }
+    }
+
+    std::cout << "min: " << min << std::endl;
+}
+
+template <typename type, int val>
+void A<type, val>::sum_numbers()
+{
+    for (int key = 0; key <= top; key++)
+    {
+        sum += arr[key];
+    }
+
+    std::cout << "sum: " << sum << std::endl;
 }
