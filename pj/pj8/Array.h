786e2b01644886ab32772ab820a599580f9ad186
Ilya193 <40058340+Ilya193@users.noreply.github.com>
2019-12-09 19:29:18 +0300

    Update Array.h

--- pj/pj8/Array.h
+++ pj/pj8/Array.h
@@ -1,236 +1,190 @@
-template <typename type, int val>
-class A
+#include <iostream>
+
+template<typename t, int value = 0>
+class v
 {
 private:
-    type arr[val];
-    type copy_arr[val];
+    t* arr;
     int top;
-    int min, max, sum, avg;
-
-    int gb;
-    int count;
-    int j;
-    int m;
+    
+    int y;
+    int val;
 
 public:
-    A();
-
-    void show() const;
-    void del(int i = val);
-    void append(int i = 0);
-    void min_number();
-    void max_number();
-    void sum_numbers();
-    void average();
-    
-    void sort();
-    void un_sort();
-    void irv(int);
-
-    int operator[](int);
-};
-
-template <typename type, int val>
-void A<type, val>::sort()
-{
-    for (int key = 0; key <= top; key++)
+    v()
     {
-        un_sort();
-    }
-}
-
-template <typename type, int val>
-void A<type, val>::un_sort()
-{
-    int num = gb + 1;
-    int c;
-
-    for (int key = 0; key < val; key++)
-    {
-        if (copy_arr[key] < num)
+        if (value == 0)
         {
-            num = copy_arr[key];
-            c = key;
+            arr = new t[1];
+            top = 1;
         }
-    }
-
-    arr[j++] = num;
-
-    irv(c);
-}
-
-template <typename type, int val>
-void A<type, val>::irv(int n)
-{
-    for (int x = 0; x <= n; x++)
-    {
-        if (x == n)
+        else
         {
-            for (int y = x; y < top; y++)
+            arr = new t[value];
+            top = value - 1;
+        }
+        
+        for (int key = 0; key <= top; key++)
+        {
+            arr[key] = 0;
+        }
+
+        val = value;
+    }
+    ~v()
+    {
+        delete[] arr;
+    }
+    
+    v& operator[](int n)
+    {
+        try
+        {
+            if (n - 1 >= top)
             {
-                copy_arr[y] = copy_arr[y + 1];
+                delete[] arr;
+                throw "element is missing";
             }
         }
-    }
-
-    copy_arr[count - 1] = gb + gb;
-    count--;
-    m--;
-}
-
-template <typename type, int val>
-int A<type, val>::operator[](int n)
-{
-    return arr[n];
-}
-
-template <typename type, int val>
-A<type, val>::A()
-{
-    top = val - 1;
-    min = max = sum = avg = 0;
-
-    type temp;
-
-    int key = 0;
-    while (key <= top && std::cin >> temp)
-    {
-        if (key <= top)
+        catch (const char* s)
         {
-            arr[key] = temp;
-            key++;
+            std::cout << s << std::endl;
+            exit(0);
         }
+        
+        y = n;
+        return *this;
+    }
+    
+    void operator=(t n)
+    {
+        arr[y] = n;
+        y = 0;
     }
 
-    for (int key = 0; key <= top; key++)
+    void del(int n = 0)
     {
-        copy_arr[key] = arr[key];
-    }
-
-    gb = 0;
-    count = val;
-    j = 0;
-    m = val;
-
-    for (int key = 0; key < val; key++)
-    {
-        if (arr[key] > gb)
+        if (n != 0)
         {
-            gb = arr[key];
-        }
-    }
-}
-
-template <typename type, int val>
-void A<type, val>::show() const
-{
-    std::cout << "arr: " << std::endl;
-    for (int key = 0; key <= top; key++)
-    {
-        std::cout << arr[key] << std::endl;
-    }
-}
-
-template <typename type, int val>
-void A<type, val>::del(int i)
-{
-    if (i > val)
-    {
-        std::cout << ">" << std::endl;
-    }
-
-    if (i == val)
-    {
-        top--;
-    }
-    else
-    {
-        i--;
-
-        for (int x = 0; x <= i; x++)
-        {
-            if (x == i)
+            if (n == value)
             {
-                for (int y = x; y <= top; y++)
+                arr[top--] = 0;
+            }
+            else
+            {
+                for (int x = 0; x < n; x++)
                 {
-                    arr[y] = arr[y + 1];
+                    if (x == n - 1)
+                    {
+                        for (int y = x; y <= top; y++)
+                        {
+                            arr[y] = arr[y + 1];
+                        }
+                    }
+                }
+
+                arr[top--] = 0;
+            }
+        }
+        else
+        {
+            arr[top--] = 0;
+        }
+    }
+
+    void append(int n)
+    {
+        try
+        {
+            if (top == value - 1)
+            {
+                delete [] arr;
+                throw "arr is full";
+            }
+        }
+        catch (const char* s)
+        {
+            std::cout << s << std::endl;
+            exit(0);
+        }
+
+        arr[++top] = n;
+    }
+
+    void sort()
+    {
+        for (int x = 0; x <= top; x++)
+        {
+            for (int y = 0; y <= top; y++)
+            {
+                if (arr[x] < arr[y])
+                {
+                    int temp = arr[y];
+                    arr[y] = arr[x];
+                    arr[x] = temp;
                 }
             }
         }
     }
-
-        top--;
-    }
-}
-
-template <typename type, int val>
-void A<type, val>::append(int i)
-{
-    if (top == val - 1)
+    
+    friend std::ostream& operator<<(std::ostream& os, v& v_)
     {
-        std::cout << "full" << std::endl;
-    }
-    else
-    {
-        top++;
-        arr[top] = i;
-    }
-}
-
-template <typename type, int val>
-void A<type, val>::max_number()
-{
-    for (int key = 0; key <= top; key++)
-    {
-        if (arr[key] > max)
+        if (v_.y == 0)
         {
-            max = arr[key];
+            for (int key = 0; key <= v_.top; key++)
+            {
+                if (key < v_.top)
+                {
+                    std::cout << v_.arr[key] << std::endl;
+                }
+                else if (key = v_.top)
+                {
+                    std::cout << v_.arr[key];
+                }
+            }
+        }
+        else
+        {
+            os << v_.arr[v_.y];
+        }
+        
+        return os;
+    }
+
+    t operator+(const v& v_)
+    {
+        int sum = 0;
+
+        for (int key = 0; key < val; key++)
+        {
+            sum += arr[key] + v_.arr[key];
+        }
+
+        return sum;
+    }
+    
+    t operator-(const v& v_)
+    {
+        int sum = 0;
+
+        for (int key = 0; key < val; key++)
+        {
+            sum += arr[key] - v_.arr[key];
+        }
+
+        return sum;
+    }
+
+    v(const v& v_)
+    {
+        arr = new t[value];
+        top = v_.top;
+        y = v_.y;
+        val = value;
+
+        for (int key = 0; key < value; key++)
+        {
+            arr[key] = v_.arr[key];
         }
     }
-
-    std::cout << "max: " << max << std::endl;
-}
-
-template <typename type, int val>
-void A<type, val>::min_number()
-{
-    min = arr[0];
-
-    for (int key = 0; key <= top; key++)
-    {
-        if (arr[key] < min)
-        {
-            min = arr[key];
-        }
-    }
-
-    std::cout << "min: " << min << std::endl;
-}
-
-template <typename type, int val>
-void A<type, val>::sum_numbers()
-{
-    for (int key = 0; key <= top; key++)
-    {
-        sum += arr[key];
-    }
-
-    std::cout << "sum: " << sum << std::endl;
-}
-
-template <typename type, int val>
-void A<type, val>::average()
-{
-    sum = 0;
-
-    for (int key = 0; key <= top; key++)
-    {
-        sum += arr[key];
-    }
-
-    avg = sum / (top + 1);
-
-    std::cout << "average: " << avg << std::endl;
-
-    sum = 0;
-}
+};
