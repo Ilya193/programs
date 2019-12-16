2856c2e0785b9c73eac470fcf2713badf553c97a
Ilya193 <40058340+Ilya193@users.noreply.github.com>
2019-12-16 07:40:26 +0300

    Update Array.h

--- pj/pj8/Array.h
+++ pj/pj8/Array.h
@@ -12,75 +12,75 @@ private:
 public:
     array(int n = 1)
     {
-      	try
-      	{
-      	    if (n == 0)
-			{
-				throw "failed to create";
-			}
-      	}
-		catch (const char* s)
-		{
-			std::cout << s << std::endl;
-			exit(0);
-		}
+        try
+        {
+            if (n == 0)
+            {
+                throw "failed to create";
+            }
+        }
+        catch (const char* s)
+        {
+            std::cout << s << std::endl;
+            exit(0);
+        }
 
         arr = new t[n];
-		top = n;
+        top = n;
     }
 
-	array(const array& array_)
-	{
-		arr = new t[array_.top];
-		top = array_.top;
-		py = array_.py;
+    array(const array& array_)
+    {
+        arr = new t[array_.top];
+        top = array_.top;
+        py = array_.py;
 
-		for (int key = 0; key < top; key++)
-		{
-			arr[key] = array_.arr[key];
-		}
-	}
+        for (int key = 0; key < top; key++)
+        {
+            arr[key] = array_.arr[key];
+        }
+    }
 
-	~array()
-	{
-		delete[] arr;
-	}
+    ~array()
+    {
+        delete[] arr;
+    }
 
-	array& operator[](int n)
-	{
-		try
-		{
-			if (n > top)
-			{
-				throw "element is missing";
-			}
-		}
-		catch (const char* s)
-		{
-			std::cout << s << std::endl;
-			exit(0);
-		}
+    array& operator[](int n)
+    {
+        try
+        {
+            if (n > top)
+            {
+                throw "element is missing";
+            }
+        }
+        catch (const char* s)
+        {
+            std::cout << s << std::endl;
+            exit(0);
+        }
 
-		py = n;
+        py = n;
 
-		return *this;
-	}
+        return *this;
+    }
 
-	void operator=(t val)
-	{
-		arr[py] = val;
-		py = -1; // output
-	}
+    void operator=(t val)
+    {
+        arr[py] = val;
+        py = -1; // output
+    }
 
-	friend std::ostream& operator<<(std::ostream& os, const array& array_)
-	{
-		if (array_.py != -1)
-		{
-			os << array_.arr[array_.py];
-		}
-		else
-		{
-			for (int key = 0; key < array_.top; key++)
+    friend std::ostream& operator<<(std::ostream& os, const array& array_)
+    {
+        if (array_.py != -1)
+        {
+            os << array_.arr[array_.py];
+        }
+        else
+        {
+            for (int key = 0; key < array_.top; key++)
             {
                 if (key < array_.top && key != array_.top - 1)
                 {
@@ -91,102 +91,102 @@ public:
                     std::cout << array_.arr[key];
                 }
             }
-		}
+        }
         
         return os;
-	}
+    }
 
-	friend std::istream& operator>>(std::istream& is, const array& array_)
-	{
-		is >> array_.arr[array_.py];
+    friend std::istream& operator>>(std::istream& is, const array& array_)
+    {
+        is >> array_.arr[array_.py];
 
-		return is;
-	}
+        return is;
+    }
 };
 
 template <typename t>
 class array_number
 {
 private:
-	t* arr;
-	int top;
-	int py;
+    t* arr;
+    int top;
+    int py;
 
-	int value;
+    int value;
 
 public:
-	array_number(int n)
-	{
-		try
-      	{
-      	    if (n == 0)
-			{
-				throw "failed to create";
-			}
-      	}
-		catch (const char* s)
-		{
-			std::cout << s << std::endl;
-			exit(0);
-		}
+    array_number(int n)
+    {
+        try
+        {
+            if (n == 0)
+            {
+                throw "failed to create";
+            }
+        }
+        catch (const char* s)
+        {
+            std::cout << s << std::endl;
+            exit(0);
+        }
 
-		value = n;
+        value = n;
         arr = new t[n];
-		top = n;
-	}
+        top = n;
+    }
 
-	array_number(const array_number& array_num_)
-	{
-		arr = new t[array_num_.top];
-		top = array_num_.top;
-		py = array_num_.py;
+    array_number(const array_number& array_num_)
+    {
+        arr = new t[array_num_.top];
+        top = array_num_.top;
+        py = array_num_.py;
 
-		for (int key = 0; key < top; key++)
-		{
-			arr[key] = array_num_.arr[key];
-		}
-	}
+        for (int key = 0; key < top; key++)
+        {
+            arr[key] = array_num_.arr[key];
+        }
+    }
 
-	~array_number()
-	{
-		delete[] arr;
-	}
+    ~array_number()
+    {
+        delete[] arr;
+    }
 
-	array_number& operator[](int n)
-	{
-		try
-		{
-			if (n > top)
-			{
-				throw "element is missing";
-			}
-		}
-		catch (const char* s)
-		{
-			std::cout << s << std::endl;
-			exit(0);
-		}
+    array_number& operator[](int n)
+    {
+        try
+        {
+            if (n > top)
+            {
+                throw "element is missing";
+            }
+        }
+        catch (const char* s)
+        {
+            std::cout << s << std::endl;
+            exit(0);
+        }
 
-		py = n;
+        py = n;
 
-		return *this;
-	}
+        return *this;
+    }
 
-	void operator=(t val)
-	{
-		arr[py] = val;
-		py = -1; // output
-	}
+    void operator=(t val)
+    {
+        arr[py] = val;
+        py = -1; // output
+    }
 
-	friend std::ostream& operator<<(std::ostream& os, const array_number& array_num_)
-	{
-		if (array_num_.py != -1)
-		{
-			os << array_num_.arr[array_num_.py];
-		}
-		else
-		{
-			for (int key = 0; key < array_num_.top; key++)
+    friend std::ostream& operator<<(std::ostream& os, const array_number& array_num_)
+    {
+        if (array_num_.py != -1)
+        {
+            os << array_num_.arr[array_num_.py];
+        }
+        else
+        {
+            for (int key = 0; key < array_num_.top; key++)
             {
                 if (key < array_num_.top && key != array_num_.top - 1)
                 {
@@ -197,51 +197,51 @@ public:
                     std::cout << array_num_.arr[key];
                 }
             }
-		}
+        }
         
         return os;
-	}
+    }
 
-	friend std::istream& operator>>(std::istream& is, const array_number& array_num_)
-	{
-		is >> array_num_.arr[array_num_.py];
+    friend std::istream& operator>>(std::istream& is, const array_number& array_num_)
+    {
+        is >> array_num_.arr[array_num_.py];
 
-		return is;
-	}
+        return is;
+    }
 
-	void del(int n = -1) // -1 flag
-	{
-		if (n == -1)
-		{
-			arr[top--] = 0;
-		}
-		else if (n != -1)
-		{
-			if (n == top)
-			{
-				arr[top--] = 0;
-			}
-			else
-			{
-				for (int x = 0; x < n; x++)
-				{
-					if (x == n - 1)
-					{
-						for (int y = x; x < top; x++)
-						{
-							arr[y] = arr[y + 1];
-						}
-					}
-				}
+    void del(int n = -1) // -1 flag
+    {
+        if (n == -1)
+        {
+            arr[top--] = 0;
+        }
+        else if (n != -1)
+        {
+            if (n == top)
+            {
+                arr[top--] = 0;
+            }
+            else
+            {
+                for (int x = 0; x < n; x++)
+                {
+                    if (x == n - 1)
+                    {
+                        for (int y = x; x < top; x++)
+                        {
+                            arr[y] = arr[y + 1];
+                        }
+                    }
+                }
 
-				arr[top--] = 0;
-			}
-		}
-	}
+                arr[top--] = 0;
+            }
+        }
+    }
 
-	void append(int n)
-	{
-		try
+    void append(int n)
+    {
+        try
         {
             if (top == value)
             {
@@ -255,12 +255,12 @@ public:
             exit(0);
         }
 
-		arr[++top] = n;
-	}
+        arr[++top] = n;
+    }
 
-	void sort()
-	{
-		for (int x = 0; x < top; x++)
+    void sort()
+    {
+        for (int x = 0; x < top; x++)
         {
             for (int y = 0; y < top; y++)
             {
@@ -272,9 +272,9 @@ public:
                 }
             }
         }
-	}
+    }
 
-	t operator+(const v& v_)
+    t operator+(const v& v_)
     {
         int sum = 0;
 
@@ -298,7 +298,7 @@ public:
         return sum;
     }
 
-	t sum()
+    t sum()
     {
         t sum = 0;
 
