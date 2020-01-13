3462139b46d10dc8b52078dead71865974590fef
Ilya193 <40058340+Ilya193@users.noreply.github.com>
2020-01-13 17:59:33 +0300

    Add files via upload

---
+++ lite/main15.cpp.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+
+template <typename t>
+class pointer_
+{
+private:
+    t* p;
+    static int count;
+
+    static void settingsi()
+    {
+        count++;
+    }
+
+    static void settingsd()
+    {
+        count--;
+    }
+
+public:
+    pointer_(t* m)
+    {
+        p = m;
+        settingsi();
+    }
+
+    pointer_(pointer_& p2)
+    {
+        p2.settingsi();
+
+        p = p2.p;
+    }
+
+    ~pointer_()
+    {   
+        if (count == 1)
+        {
+            delete[] p;
+        }
+        else
+        {
+            settingsd();
+        }
+    }
+
+    t* operator&() const
+    {
+        return p;
+    }
+
+    t& operator*() const
+    {
+        return *p;
+    }
+
+    void operator=(t n)
+    {
+        *p = n;
+    }
+
+    t* operator->() const
+    {
+        return p;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, pointer_& point)
+    {
+        os << *point.p;
+
+        return os;
+    }
+};
+
+template <typename t>
+int pointer_<t>::count = 0;
+
+int main()
+{
+    
+    
+    return 0;
+}
