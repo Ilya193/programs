17cd7b2da7ef410d6046513aa484d7abb62af047
Ilya193 <40058340+Ilya193@users.noreply.github.com>
2020-04-21 18:14:54 +0300

    Update main13.cpp

--- lite/main13.cpp
+++ lite/main13.cpp
@@ -1,39 +1,60 @@
 #include <iostream>
 
-int f(int);
+int fun(int, int*);
 
 int main()
 {
-    int main_number = 1000;
-    int count = 0;
-    int num = 0;
+    int* numbers;
+    int main_number, num = 0, count = 0;
+    std::cin >> main_number;
+    std::cin.get();
 
-    for (int key = 3; key < main_number; key++)
+    if (main_number >= 3)
     {
-        num = f(key);
+        numbers = new int[main_number];
 
-        if (num == 0)
+        for (int key = 3; key < main_number; key++)
         {
-            count++;
+            num = fun(key, numbers);
+
+            if (num != 0)
+            {
+                count++;
+            }
+        }
+    }
+    else
+    {
+        std::cout << "false" << std::endl;
+    }
+
+    for (int key = 0; key < count; key++)
+    {
+        std::cout << numbers[key] << std::endl;
+    }
+}
+
+int fun(int m, int* nr)
+{
+    static int i = 0;
+    int j = 0, z = 0, a = 0;
+
+    for (int x = 2; x < m; x++)
+    {
+        if (m % 1 == 0 && m % m == 0 && m % x != 0)
+        {
+            z++;
+        }
+        if (m % 1 == 0 && m % m == 0 && m % x == 0)
+        {
+            a++;
         }
     }
 
-    std::cout << count << std::endl;
-
-    return 0;
-}
-
-int f(int m)
-{
-    int j = 0;
-
-    for (int x = 1; x < m; x++)
+    if (z >= 0 && a == 0)
     {
-        if (x != 1 && x != m && m % x == 0)
-        {
-            j = 10; // flag
-            break;
-        }
+        j++;
+        nr[i++] = m;
     }
 
     return j;
